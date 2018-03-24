1d54cece2c935303b38c5e10df63cb745eb0b2ed
Derek Lam <dc.lam@mail.utoronto.ca>
2018-03-24 18:52:17 -0400

    Working ATmega328 data logger + new trigger scheme

--- atmega328/init/firmware/main.c
+++ atmega328/init/firmware/main.c
@@ -21,7 +21,6 @@ volatile uint8_t buff_full = 0;
 volatile uint8_t triggered = 0;
 // volatile uint8_t on = 0; // consult OC0A
 
-volatile uint8_t pwm;
 volatile uint8_t phase = 2;
 
 const uint8_t PHASES[] = {
@@ -32,7 +31,7 @@ const uint8_t PHASES[] = {
 	// OFF_MASK
 };
 const uint8_t adc[] = {
-	1, 1, 2
+	4, 4, 5
 };
 const uint8_t RESTING[] = {
 	0b01010100,
@@ -41,31 +40,34 @@ const uint8_t RESTING[] = {
 };
 
 void put_commutation(uint8_t c) {
-	ATOMIC_BLOCK(ATOMIC_FORCEON) {
-		PORTD = (PORTD & ~(0b111 << 5)) | ((c & 0b111) << 5);
+	// ATOMIC_BLOCK(ATOMIC_FORCEON) {
+		PORTC = (PORTC & ~(0b111)) | (c & 0b111);
 		PORTB = (PORTB & ~(0b111)) | ((c >> 3) & 0b111);
-	}
+	// }
 }
 
 ISR(TIMER0_OVF_vect) {
+	phase = (phase + 1) % 3;
+	
+	TIMSK1 &= ~(1 << 5); // disable input capture interrupt
+	ADMUX = (ADMUX & ~(0b11)) | adc[phase];
+	
 	put_commutation(OFF_MASK); // de-energize for a bit
 	if(!buff_full)
 		put_commutation(PHASES[phase]);
 	
+	TIMSK1 |= 1 << 5;
+	
 	TCNT1 = 0; // reset threshold timer
 	TIFR0 |= 1;
 	triggered = 0;
-	pwm++;
 	
-	ADMUX = (ADMUX & ~(0b11)) | adc[phase];
 }
 ISR(TIMER0_COMPA_vect) {
 	put_commutation(RESTING[phase]);
-	phase = (phase + 1) % 3;
 }
 
 // void quadrature(uint8_t vec) {
-	
 // 	last_int = vec;
 // 	// PORTB ^= 1; // signal threshold
 // }
@@ -74,7 +76,7 @@ volatile uint8_t last_Q0 = 0;
 volatile uint8_t last_Q90 = 0;
 
 ISR(INT0_vect) {
-	uint8_t current_Q0 = (PORTD & 0b100) >> 2;
+	uint8_t current_Q0 = (PIND & 0b100) >> 2;
 	if(last_Q0 != current_Q0) {
 		if(last_Q0 != last_Q90)
 			encoder++;
@@ -85,8 +87,8 @@ ISR(INT0_vect) {
 	EIFR |= 1;
 }
 ISR(INT1_vect) {
-	uint8_t current_Q90 = (PORTD & 0b1000) >> 3;
-	if(last_Q0 != current_Q90) {
+	uint8_t current_Q90 = (PIND & 0b1000) >> 3;
+	if(last_Q90 != current_Q90) {
 		if(last_Q90 == last_Q0)
 			encoder++;
 		else
@@ -96,20 +98,19 @@ ISR(INT1_vect) {
 	EIFR |= 0b10;
 }
 
-ISR(ANALOG_COMP_vect) {
-	if(pwm % PWM_CYCLE_SKIP == 0 && !triggered) {
+// ISR(ANALOG_COMP_vect) {
+ISR(TIMER1_CAPT_vect) {
+	if(!triggered) {
 		PORTB ^= 0b1000;
-		// ATOMIC_BLOCK(ATOMIC_FORCEON) {
-			if(buff_idx < BUFF_SIZE) {
-				BUFF[buff_idx++] = TCNT1; // stash threshold time
-				BUFF[buff_idx++] = encoder; // stash encoder value
-				BUFF[buff_idx++] = phase;
-			}
-			else {
-				buff_full = 1;
-				// OCR0A = 0;
-			}
-		// }
+		if(buff_idx < BUFF_SIZE) {
+			BUFF[buff_idx++] = ICR1; // stash threshold time
+			BUFF[buff_idx++] = (phase << 12) | (encoder & 0xFFF); // stash encoder value
+		}
+		else {
+			buff_full = 1;
+			// OCR0A = 0;
+		}
+		
 		triggered = 1;
 	}
 	ACSR |= 1 << 4;
@@ -136,8 +137,8 @@ ISR(ANALOG_COMP_vect) {
 // }
 
 void port_init() {
-	DDRD = 0xF0; // output all except TxRx and encoder
-	DDRB = 0x0F | (1 << 5); // output all motor phases and on pin 13
+	DDRC = 0b111; // output C0-C2 (motor phases)
+	DDRB = 0x0F | (1 << 5); // output all motor phases, indicator and on pin 13
 	PORTB &= ~(1 << 5);
 	
 	EICRA = 0b01 << 2 | // logic-level change on INT1 generates INT1
@@ -149,22 +150,24 @@ void timer_init() {
 	         0b00 << 4 | // don't touch OC0B
 	         0b11; // fast PWM (needed to detect on-off cycles)
 	TCCR0B = 0 << 3 | // fast PWM (continued)
-	         0b011; // 64x prescaling (1024Hz)
-	OCR0A = 42;
+	         0b101; // 1024x prescaling (64Hz)
+	OCR0A = 1; // 3/256 duty: 244us pulse width
 	TIMSK0 = 0b011; // enable compare match A; enable overflow interrupt
 	
 	TCCR1A = 0b00 << 6 | // don't touch OC0A
 	         0b00 << 4 | // don't touch OC0B
 	         0b00000; // Normal mode
-	TCCR1B |= 0b01; // no prescaling
+	TCCR1B |= 0b11 << 6 | // input capture: rising edge with noise cancellation
+	          0b01; // no prescaling
+	TIMSK1 = 1 << 5; // enable input capture interrupt
 }
 void ac_init() {
 	// ADC0 implicitly from ADMUX & 0xF = 0
 	ADCSRB = 1 << 6 | // ADC0 input to comparator
 	         0b000; // free-running mode
-	ACSR = 1 << 6 | // use internal bandgap (assume 1.136V)
-	       1 << 3 | // enable interrupts
-	       0b11; // toggle triggers interrupt
+	ACSR = // 1 << 3 | // enable interrupts
+	       1 << 2; // | // enable AC input capture
+	       // 0b11; // toggle triggers interrupt
 }
 
 int main(void)
